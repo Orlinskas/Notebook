55e20a1a62bc74d13af60de5146de55aa2deb123
orlinskas <orlinskaslove@gmail.com>
2019-11-07 15:45:09 +0200

    apk v1.1

--- app/src/main/java/com/orlinskas/notebook/notificationHelper/NotificationAlarmReceiver.java
+++ app/src/main/java/com/orlinskas/notebook/notificationHelper/NotificationAlarmReceiver.java
@@ -1,13 +1,20 @@
 package com.orlinskas.notebook.notificationHelper;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class NotificationAlarmReceiver extends BroadcastReceiver {
+import androidx.legacy.content.WakefulBroadcastReceiver;
+
+import sm.euzee.github.com.servicemanager.ServiceManager;
+
+public class NotificationAlarmReceiver extends WakefulBroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
-        int id = intent.getIntExtra("ID", 1);
-        context.startService(new Intent(context, NotificationShowService.class).putExtra("ID", id));
+    public void onReceive(final Context context, Intent intent) {
+        final int id = intent.getIntExtra("ID", 1);
+
+        ServiceManager.runService(context, () -> {
+            NotificationStarter starter = new NotificationStarter();
+            starter.start(context, id);
+        }, true);
     }
 }
