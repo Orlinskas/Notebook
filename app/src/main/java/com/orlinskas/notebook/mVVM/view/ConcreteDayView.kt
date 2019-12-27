f24d1ccb50a3d2c9f919b73df7a176f0d628995f
Vladislav Orlinskas <vladislav.orlinskas@branderstudio.com>
2019-12-27 16:37:38 +0200

    concreteDay переведен

---
+++ app/src/main/java/com/orlinskas/notebook/mVVM/view/ConcreteDayView.kt
@@ -0,0 +1,109 @@
+package com.orlinskas.notebook.mVVM.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.orlinskas.notebook.Constants.*
+import com.orlinskas.notebook.Enums
+import com.orlinskas.notebook.R
+import com.orlinskas.notebook.builder.ToastBuilder
+import com.orlinskas.notebook.mVVM.fragment.DayFragmentActions
+import com.orlinskas.notebook.mVVM.fragment.DayView
+import com.orlinskas.notebook.mVVM.model.Notification
+import com.orlinskas.notebook.mVVM.viewModel.ConcreteDayViewModel
+import kotlinx.android.synthetic.main.activity_concrete_day.*
+
+class ConcreteDayView : AppCompatActivity(), DayFragmentActions {
+    private lateinit var progressBar: ProgressBar
+    private var dayID: Int = 0
+    private lateinit var model: ConcreteDayViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_concrete_day)
+
+        progressBar = findViewById(R.id.activity_concrete_day_pb)
+        activity_concrete_day_fab.setOnClickListener {
+            openCreateNotificationActivity()
+        }
+
+        model = ViewModelProviders.of(this).get(ConcreteDayViewModel::class.java)
+
+        model.daysData.observe(this, Observer {
+            updateUI()
+        })
+
+        model.repositoryStatusData.observe(this, Observer {
+            when (it) {
+                Enums.RepositoryStatus.LOADING -> progressBar.visibility = View.VISIBLE
+                Enums.RepositoryStatus.READY -> progressBar.visibility = View.INVISIBLE
+            }
+        })
+
+        model.connectionStatusData.observe(this, Observer {
+            when (it) {
+                Enums.ConnectionStatus.CONNECTION_DONE -> doToast(REMOTE)
+                Enums.ConnectionStatus.CONNECTION_FAIL -> doToast(LOCAL)
+            }
+        })
+
+        if (savedInstanceState != null) {
+            dayID = savedInstanceState.getInt(DAY_ID)
+        }
+        else {
+            intent.extras?.let {
+                dayID = it.getInt(DAY_ID)
+            }
+        }
+    }
+
+    private fun updateUI() {
+        val fm = supportFragmentManager
+
+        var fragment = fm.findFragmentById(R.id.activity_concrete_day_container)
+        if (fragment == null) {
+            val bundle = Bundle()
+            bundle.putInt(DAY_ID, dayID)
+            bundle.putBoolean(IS_FULL_DISPLAY, true)
+
+            fragment = DayView()
+            fragment.arguments = bundle
+
+            fm.beginTransaction()
+                    .add(R.id.activity_concrete_day_container, fragment)
+                    .commit()
+        } else {
+            fm.beginTransaction().detach(fragment).attach(fragment).commit()
+        }
+    }
+
+    private fun doToast(message: String) {
+        if (!isFinishing) {
+            runOnUiThread { ToastBuilder.doToast(application.applicationContext, message) }
+        }
+    }
+
+    private fun openCreateNotificationActivity() {
+        val intent = Intent(applicationContext, CreateNotificationView::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+        intent.action = AFTER_CREATE_OPEN_DAY
+        startActivity(intent)
+    }
+
+    override fun openDay(dayID: Int) {
+        //not specified in the technical specifications for this window
+    }
+
+    override fun deleteNotification(notification: Notification) {
+        model.deleteNotification(notification)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(DAY_ID, dayID)
+    }
+}
